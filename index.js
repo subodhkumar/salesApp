2d2fcddc1fb68d9858424f5d6f99113334181472
subodhkumar <subodhkumarjc@gmail.com>
2018-02-28 11:11:30 +0530

    Port changes to Heroku

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+const PORT = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
   res.redirect('/views/home.html');
 });
-app.listen(4000,()=>{
+app.listen(PORT,()=>{
     console.log('Server started at 4000');
 })
